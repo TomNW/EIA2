7bab46d3f95a35e536c37faa7c7d1063bbe7686b
TomNW <tomnoahwiesmann@gmail.com>
2023-03-23 12:53:05 +0100

    config über cmd

---
+++ A01_Poem/main.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+var subjekt = ["Beethoven", "Biggie", "Mozart", "2pac",];
+var praedikat = ["ruft", "wohnt bei", "hasst", "fürchtet"];
+var objekt = ["PSV Eindhoven", "Dracula", "Schule", "Fallobst"];
+var length = 4;
+for (var index = 0; index < subjekt.length; index++) {
+}
+function getVerse(_subject, _P, _O) {
+    var Verse = _subject + _P + _O;
+}
---
+++ A01_Poem/main.ts
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+let subjekt: string[] = ["Beethoven", "Biggie", "Mozart", "2pac",];
+let praedikat: string[] = ["ruft", "wohnt bei", "hasst", "fürchtet"];
+let objekt: string[] = ["PSV Eindhoven", "Dracula", "Schule", "Fallobst"];
+
+var length: number = 4
+
+for (let index = 0; index < subjekt.length; index++) {
+
+}
+
+function getVerse(_subjekt: string, _praedikat: string, _objekt: string) {
+
+    let Verse: string = _subjekt + _praedikat + _objekt
+}
