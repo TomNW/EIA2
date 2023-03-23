508be8a54be4c58a32b497535772d135deb97780
TomNW <tomnoahwiesmann@gmail.com>
2023-03-23 13:00:27 +0100

    ts for schleife

--- A01_Poem/main.ts
+++ A01_Poem/main.ts
@@ -4,13 +4,13 @@ let subjekt: string[] = ["Beethoven", "Biggie", "Mozart", "2pac",];
 let praedikat: string[] = ["ruft", "wohnt bei", "hasst", "fürchtet"];
 let objekt: string[] = ["PSV Eindhoven", "Dracula", "Schule", "Fallobst"];
 
-var length: number = 4
 
-for (let index = 0; index < subjekt.length; index++) {
+for (let index = 4; index > 1; index--) {
 
-}
-
-function getVerse(_subjekt: string, _praedikat: string, _objekt: string) {
+    function getVerse(_subjekt: string, _praedikat: string, _objekt: string) {
 
     let Verse: string = _subjekt + _praedikat + _objekt
+    console.log("Alohomora")
 }
+}
+
