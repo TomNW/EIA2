39150a10b8540c1cda15fb346149272a7974729d
TomNW <tomnoahwiesmann@gmail.com>
2023-03-23 12:34:31 +0100

    ts hat sub,prä und obs

--- A01_Poem/ts/main.ts
+++ A01_Poem/ts/main.ts
@@ -1,1 +1,12 @@
 console.log("Hello");
+
+let Subjekt[]:string = ["Beethoven","Biggie","Mozart","2pac",];
+let Praedikat[]:string = ["ruft","wohnt bei","hasst","fürchtet"];
+let Objekt[]:string = ["PSV Eindhoven","Dracula","Schule","Fallobst"];
+
+var length:number = 4
+for (length>=1) {
+
+length--
+
+}
