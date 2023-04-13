72a8e70216e935c94447e0efb557d56977162625
TomNW <tomnoahwiesmann@gmail.com>
2023-04-13 10:57:40 +0200

    Task wird als Text eingefügt

--- A04_WebApp/main.js
+++ A04_WebApp/main.js
@@ -3,10 +3,14 @@ Aufgabe: L03_Aufgabenliste_Formular
 Name: Tom Wiesmann
 Matrikel: 272715
 Datum: 08.04.2023
-Quellen: EIA 1 ToDoListen Aufgabe
+Quellen: EIA 1 ToDoListen Aufgabe, stackoverflow.com
 */
 var WebApp;
 (function (WebApp) {
+    var task;
+    var addButtonDOMElement;
+    var todosDOMElement;
+    var counterDOMElement;
     window.addEventListener("load", loadApp);
     function loadApp() {
         let task = document.querySelector("#input-Task");
@@ -14,10 +18,11 @@ var WebApp;
         let date = document.querySelector("#input-Date");
         let comment = document.querySelector("#input-Comment");
         let addButton = document.querySelector("#addButton");
+        console.log(task);
         addButton.addEventListener("click", postTask);
-    }
-    function postTask() {
-        console.log("test1");
+        function postTask() {
+            document.getElementById("demo").innerHTML = task.value + "</br>" + name.value + "</br>" + date.value + "</br>" + comment.value;
+        }
     }
 })(WebApp || (WebApp = {}));
 //# sourceMappingURL=main.js.map
--- A04_WebApp/main.ts
+++ A04_WebApp/main.ts
@@ -3,29 +3,43 @@ Aufgabe: L03_Aufgabenliste_Formular
 Name: Tom Wiesmann
 Matrikel: 272715
 Datum: 08.04.2023
-Quellen: EIA 1 ToDoListen Aufgabe
+Quellen: EIA 1 ToDoListen Aufgabe, stackoverflow.com
 */
 
 namespace WebApp{
+
+var task: HTMLInputElement;
+var addButtonDOMElement: HTMLElement;
+var todosDOMElement: HTMLElement;
+var counterDOMElement: HTMLElement;    
 
 window.addEventListener("load", loadApp)
 
 function loadApp() {
 
-let task: HTMLElement = <HTMLElement>document.querySelector("#input-Task")
-let name: HTMLElement = <HTMLElement>document.querySelector("#input-Name")
-let date: HTMLElement = <HTMLElement>document.querySelector("#input-Date")
-let comment: HTMLElement = <HTMLElement>document.querySelector("#input-Comment")
-let addButton: HTMLElement = <HTMLElement>document.querySelector("#addButton")
+let task = <HTMLInputElement>document.querySelector("#input-Task");
+let name = <HTMLInputElement>document.querySelector("#input-Name");
+let date = <HTMLInputElement>document.querySelector("#input-Date");
+let comment = <HTMLInputElement>document.querySelector("#input-Comment");
+let addButton = <HTMLInputElement>document.querySelector("#addButton");
 
+console.log(task)
 addButton.addEventListener("click", postTask)
-
-}
-
-
 function postTask (){
-    
-console.log("test1")
+  
+document.getElementById("demo").innerHTML = task.value +"</br>"+ name.value +"</br>"+ date.value +"</br>"+ comment.value;
+
+
+      
+
+
+
+
+
+
+
+
+
 
 
 
@@ -34,6 +48,11 @@ console.log("test1")
 
 
 }
+}
+
+
+
+
 
 
 
